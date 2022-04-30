51555be8088934203889b1a8dc2fc36389ac9572
John-Pena <jmp1495@gmail.com>
2022-04-30 10:40:30 -0500

    added foreign keys to database tables to references each other

--- db/schema.sql
+++ db/schema.sql
@@ -3,21 +3,24 @@ DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS employee;
 
 CREATE TABLE department (
-  id INTEGER PRIMARY KEY,
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(30)
 );
 
 CREATE TABLE role (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(30),
   salary DECIMAL,
-  department_id INT
+  department_id INT,
+  FOREIGN KEY (department_id) references department(id)
 );
 
 CREATE TABLE employee (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
-  manager_id INT
+  manager_id INT,
+  FOREIGN KEY (role_id) references role(id),
+  FOREIGN KEY (manager_id) references employee(id)
 );
